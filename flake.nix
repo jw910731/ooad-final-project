3d47df843adff62cb5e92b693549ba4d9987d575
jw910731 <jw910731@gmail.com>
2025-04-15 21:48:16 +0800

    Add laravel template

--- flake.nix
+++ flake.nix
@@ -7,40 +7,74 @@
   };
 
   nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-trusted-public-keys =
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
-    let
-      forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in
-    {
+  outputs = { self, nixpkgs, devenv, systems, ... }@inputs:
+    let forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in {
       packages = forEachSystem (system: {
         devenv-up = self.devShells.${system}.default.config.procfileScript;
         devenv-test = self.devShells.${system}.default.config.test;
       });
 
-      devShells = forEachSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                {
-                  # https://devenv.sh/reference/options/
-                  packages = [ ];
-                  languages.java = {
-                    enable = true;
-                    gradle.enable = true;
-                    jdk.package = pkgs.jdk21_headless;
+      devShells = forEachSystem (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              ({ pkgs, config, ... }: {
+                # https://devenv.sh/reference/options/
+                packages = [ ];
+
+                env.DATABASE_URL = "pgsql://app:app@localhost/app";
+
+                languages.javascript = {
+                  enable = true;
+                };
+                languages.php = {
+                  enable = true;
+                  package = pkgs.php.buildEnv {
+                    extensions = { all, enabled }:
+                      with all;
+                      enabled ++ [ xdebug redis ];
+                    extraConfig = ''
+                      memory_limit=256m
+                    '';
                   };
-                }
-              ];
-            };
-          });
+                  fpm.pools.web = {
+                    settings = {
+                      "clear_env" = "no";
+                      "pm" = "dynamic";
+                      "pm.max_children" = 10;
+                      "pm.start_servers" = 2;
+                      "pm.min_spare_servers" = 1;
+                      "pm.max_spare_servers" = 10;
+                    };
+                  };
+                };
+                services.caddy.enable = true;
+                services.caddy.virtualHosts.":8000" = {
+                  extraConfig = ''
+                    root * public
+                    php_fastcgi unix/${config.languages.php.fpm.pools.web.socket}
+                    file_server
+                  '';
+                };
+                services.postgres = {
+                  enable = true;
+                  initialDatabases = [{
+                    name = "app";
+                    user = "app";
+                    pass = "app";
+                  }];
+                };
+              })
+            ];
+          };
+        });
     };
 }
