b375b66bacefd465122b2aebd149d0eaf5761873
a5121278 <5121278@gmail.com>
2025-06-02 22:54:09 +0800

    feat: now can choose multiple users when course create

--- resources/views/livewire/courses/create.blade.php
+++ resources/views/livewire/courses/create.blade.php
@@ -12,22 +12,45 @@ new class extends Component {
     #[Validate('required|string')]
     public $description = '';
 
-    #[Validate('required|exists:users,id')]
-    public $user_id = null;
+    #[Validate([
+        'userTeacher_id' => 'required',
+        'userTeacher_id.*' => 'exists:users,id',
+    ])]
+    public $userTeacher_id = [];
+
+    public $userTA_id = [];
+    public $userStudent_id = [];
 
     public function create(): void
     {
         $validated = $this->validate();
-        $user = User::find($validated['user_id']);
-        $user->courses()->create([
+        $course = Course::create([
             'title' => $validated['title'],
             'description' => $validated['description']
-        ], ['role' => 'teacher']);
+        ]);
+        $course->users()->attach(
+            array_map(function($value) use (&$validated) {
+                return ['role' => 'teacher'];
+            }, array_flip($validated['userTeacher_id']))
+        );
+        $course->users()->attach(
+            array_map(function($value) use (&$validated) {
+                return ['role' => 'teaching_assistant'];
+            }, array_flip($this->userTA_id))
+        );
+        $course->users()->attach(
+            array_map(function($value) use (&$validated) {
+                return ['role' => 'student'];
+            }, array_flip($this->userStudent_id))
+        );
         $this->redirectRoute('courses.index');
     }
 }
 
 ?>
+
+
+
 <flux:container>
     <div class="flex w-full flex-col gap-2">
         <div class="relative mb-6 w-full">
@@ -39,16 +62,37 @@ new class extends Component {
                 <flux:input class="mb-6" wire:model="title" :label="__('Course Title')" required
                             autofocus/>
                 <flux:textarea class="mb-6" wire:model="description" :label="__('Course Description')"/>
-                <x-select
-                    label="Teacher of the Course"
-                    placeholder="Select user as teacher"
-                    :async-data="[
-                        'api' => route('userSearch.search'),
-                        'credentials' => 'include',
-                    ]"
-                    option-label="name"
-                    option-value="id"
-                    wire:model="user_id"/>
+                <x-select multiselect
+                          label="Teacher of the Course"
+                          placeholder="Select user as teacher"
+                          :async-data="[
+                            'api' => route('userSearch.search'),
+                            'credentials' => 'include',
+                            ]"
+                          option-label="name"
+                          option-value="id"
+                          wire:model="userTeacher_id"/>
+                <x-select multiselect
+                          label="Teaching assistant of the Course"
+                          placeholder="Select users as teaching assistant"
+                          :async-data="[
+                                'api' => route('userSearch.search'),
+                                'credentials' => 'include',
+                            ]"
+                          option-label="name"
+                          option-value="id"
+                          wire:model="userTA_id"/>
+                <x-select multiselect
+                          label="Students of the Course"
+                          placeholder="Select users as students"
+                          :async-data="[
+                                'api' => route('userSearch.search'),
+                                'credentials' => 'include',
+                            ]"
+                          option-label="name"
+                          option-value="id"
+                          wire:model="userStudent_id"/>
+
                 <div class="mb-6">
                     <flux:button variant="primary" type="submit" class="w-full">{{ __('Create') }}</flux:button>
                 </div>
