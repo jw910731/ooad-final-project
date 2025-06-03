51c2ebf0d511f6c4e78a9b203e875105d4a9d6cf
jw910731 <jw910731@gmail.com>
2025-06-04 00:46:18 +0800

    Fix create course feature test

--- tests/Feature/CourseTest.php
+++ tests/Feature/CourseTest.php
@@ -17,9 +17,17 @@ class CourseTest extends TestCase
         $user = User::factory()->create([
             'system_admin' => true,
         ]);
+        $get_id = function($user){
+            return $user->id;
+        };
+        $teachers = User::factory()->count(3)->create()->map($get_id)->toArray();
+        $students = User::factory()->count(3)->create()->map($get_id)->toArray();
+        $teaching_assistants = User::factory()->count(3)->create()->map($get_id)->toArray();
         $this->actingAs($user);
         $response = Volt::test('courses.create')
-            ->set('user_id', $user->id)
+            ->set('userTeacher_id', $teachers)
+            ->set('userStudent_id', $students)
+            ->set('userTA_id', $teaching_assistants)
             ->set('title', 'test')
             ->set('description', 'test')
             ->call('create');
